310b7b5aede3649f7b13f9cd91b6c1554abf0450
claudiuj <jclauc@gmail.com>
2023-06-18 12:15:49 +0300

    project routes fix

--- src/routes/projects.ts
+++ src/routes/projects.ts
@@ -6,11 +6,11 @@ import { adminCheck } from '../middleware/adminCheck';
 export const projects: Router = Router();
 
 projects.get('/getprojects', authToken, getProjects);
-projects.get('/getuserprojects', authToken, getUserProjects)
+projects.get('/getuserprojects/:userId', authToken, getUserProjects)
 projects.post('/addproject', authToken, adminCheck, addProject);
 projects.put('/updateproject/:projectId', authToken, adminCheck, updateProject);
 projects.delete('/removeproject/:projectId', authToken, adminCheck, removeProject);
 projects.get('/getprojecttypes', authToken, adminCheck, getProjectTypes);
 projects.post('/addusertoproject', authToken, adminCheck, addUserToProject);
 projects.delete('/removeuserfromproject/:userId/:projectId', authToken, adminCheck, removeUserFormProject);
-projects.get('/getprojectdetails', authToken, adminCheck, getProjectDetails);
+projects.get('/getprojectdetails', authToken, getProjectDetails);
